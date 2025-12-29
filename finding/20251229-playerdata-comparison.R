bed76e1484aa107bc6f506a0f3bd002c74f2aa22
ChaneyYao <chaney0819@gmail.com>
2025-12-29 23:32:47 +0800

    Update view of emerging player comps

--- finding/20251229-playerdata-comparison.R
+++ finding/20251229-playerdata-comparison.R
@@ -1,3 +1,4 @@
+rm(list = ls())
 library(httr)
 library(jsonlite)
 library(dplyr)
@@ -10,12 +11,26 @@ now_source = "C:/Teamfight-Tactics/Teamfight-Tactics/data/processed/20251229_all
 previous <- readRDS(previous_source)
 now = readRDS(now_source)
 
-data = now$tw2$data %>%
-  mutate(previous_rank = rank) %>%
+emerging_players = now$tw2$data %>%
+  mutate(now_rank = rank) %>%
   inner_join(previous$tw2$data %>%
-               mutate(now_rank = rank) %>%
-               select(player_id, now_rank), 
+               mutate(previous_rank = rank) %>%
+               select(player_id, previous_rank), 
              by = "player_id") %>%
-  select(riot_id, previous_rank, now_rank) %>%
+  select(player_id, riot_id, previous_rank, now_rank) %>%
   mutate(delta = now_rank - previous_rank) %>%
-  filter(delta < -100)
+  filter(delta < -100) %>%
+  arrange(now_rank)
+
+ori = now$tw2$data
+
+emerging_strat = emerging_players %>%
+  inner_join(now$tw2$data %>%
+               select(-c(riot_id, puuid, rating, rating_numeric, live, summoner_region, num_played, rank)), by = "player_id") 
+
+# Create a standalone lookup table
+carries_lookup <- emerging_strat %>%
+  select(riot_id, stats) %>%
+  mutate(carries = stats$RecentResult$topCarries) %>% # Dig directly to the list
+  select(riot_id, carries) %>%
+  unnest(carries)
